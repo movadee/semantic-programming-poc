46ade73cb8c8af3a5fa381fad6769c5d1bc8a29f
alla_abramova <alla.abramova@capco.com>
2018-01-23 15:00:08 -0500

    adding @angular-devkit/core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// material design
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
+// components
 import { AppComponent } from './app.component';
 
 
@@ -10,7 +14,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
+    // material design modules
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
